470d087171121ee6ef147ff24cf3528edc83f12a
KO <kamil.orlicki@outlook.com>
2020-11-26 00:22:45 +0100

    routing and rendering

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
 
-echo 'Hi there 👋';
+require "Routing.php";
+
+$path = trim($_SERVER['REQUEST_URI'], '/');
+$path = parse_url($path, PHP_URL_PATH);
+
+Router::get('index', 'DefaultController');
+Router::get('main', 'DefaultController');
+Router::get('user', 'DefaultController');
+Router::get('search', 'DefaultController');
+Router::get('statistics', 'DefaultController');
+
+Router::run($path);
---
+++ src/controllers/DefaultController.php
@@ -0,0 +1,26 @@
+<?php 
+
+require_once 'AppController.php';
+
+class DefaultController extends AppController {
+    
+    public function index() {
+        $this->render('login');
+    }
+
+    public function main() {
+        $this->render('main');
+    }
+
+    public function user() {
+        $this->render('user');
+    }
+
+    public function search() {
+        $this->render('search');
+    }
+
+    public function statistics() {
+        $this->render('statistics');
+    }
+}
